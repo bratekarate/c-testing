6f6af2cd65ff8a00538de4e40224c5f9048429db
bratekarate <bratekannkarate@gmail.com>
2021-01-20 02:29:29 +0100

    cleanup matrix session extractor

--- src/matrix_session_extract.c
+++ src/matrix_session_extract.c
@@ -1,23 +1,39 @@
 #include <Python.h>
 #include <inttypes.h>
 #include <locale.h>
+#include <openssl/aes.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-void print_bytes(char *bytes, int len);
-void print_hex_bytes(char *bytes, int len);
-void print_uint8(u_int8_t *bytes, int len);
-const char *calc_aes_key(char *passphrase, int rounds, char *salt);
+#define SALT_LEN (16)
+#define VECTOR_LEN (16)
+#define ROUNDS_LEN (4)
+#define HMAC_SHA256_LEN (32)
+#define AES_KEY_LEN (64)
+
+void print_bytes(const char *bytes, const size_t len);
+void print_hex_bytes(const char *bytes, const size_t len);
+void print_uint8(const u_int8_t *bytes, const size_t len);
+const unsigned char *calc_aes_key(const char *passphrase, const size_t rounds,
+                                  const char *salt);
 
 int main(int argc, char *argv[]) {
 
   FILE *fp;
 
-  if (argc < 2) {
+  switch (argc) {
+  case 3:
     fp = stdin;
-  } else {
-    fp = fopen(argv[1], "r");
+    break;
+  case 4:
+    fp = fopen(argv[3], "r");
+    break;
+  default:
+    fprintf(stderr,
+            "Error: Need at least a passphrase and an output filepath.\n");
+    exit(1);
+    break;
   }
 
   if (fp == NULL) {
@@ -25,26 +41,24 @@ int main(int argc, char *argv[]) {
     return 1;
   }
 
-  char format = fgetc(fp);
+  const char format = fgetc(fp);
 
-  int i;
-  char salt[16];
-  for (i = 0; i < 16; i++) {
+  size_t i;
+  char salt[SALT_LEN + 1];
+  for (i = 0; i < SALT_LEN; i++) {
     salt[i] = fgetc(fp);
   }
+  salt[i] = '\0'; // \0 character necessary for python object conversion
 
-  char vec[16];
-  for (i = 0; i < 16; i++) {
+  char vec[VECTOR_LEN];
+  for (i = 0; i < VECTOR_LEN; i++) {
     vec[i] = fgetc(fp);
   }
 
-  u_int8_t rnd_arr[4];
-  for (i = 0; i < 4; i++) {
+  u_int8_t rnd_arr[ROUNDS_LEN];
+  for (i = 0; i < ROUNDS_LEN; i++) {
     rnd_arr[i] = fgetc(fp);
   }
-
-  uint32_t rounds =
-      (rnd_arr[0] << 24) + (rnd_arr[1] << 16) + (rnd_arr[2] << 8) + rnd_arr[3];
 
   i = 0;
   char *rest = malloc(0);
@@ -54,26 +68,64 @@ int main(int argc, char *argv[]) {
     rest[i] = fgetc(fp);
     i++; // TODO: why is i after loop the size of rest bytes + 1?
   }
-  i--;
 
   fclose(fp);
 
-  char hmac_sha256[32];
-  int j;
-  for (j = 0; j < 32; j++) {
-    hmac_sha256[32 - 1 - j] = rest[i - 1 - j];
+  if (i < HMAC_SHA256_LEN + 1) {
+    fprintf(stderr, "Error: File content is too short.");
+    exit(1);
   }
 
-  int rest_size = i;
+  i--; // see TODO above
+
+  char hmac_sha256[HMAC_SHA256_LEN];
+  size_t j;
+  for (j = 0; j < HMAC_SHA256_LEN; j++) {
+    hmac_sha256[HMAC_SHA256_LEN - 1 - j] = rest[i - 1 - j];
+  }
+
   i -= j;
 
-  const char *aes_key = calc_aes_key(argv[2], rounds, salt);
+  fp = fopen(argv[1], "w");
+  for (size_t k = 0; k < i; k++) {
+    fputc(rest[k], fp);
+  }
 
-  printf("%s\n", aes_key);
+  fclose(fp);
+
+  const uint32_t rounds =
+      (rnd_arr[0] << 24) + (rnd_arr[1] << 16) + (rnd_arr[2] << 8) + rnd_arr[3];
+
+  const unsigned char *aes_key = calc_aes_key(argv[2], rounds, salt);
+
+  // print resulting key and initialization vector
+  for (size_t c = 0; c < AES_KEY_LEN; c++) {
+    printf("%02x", aes_key[c]);
+  }
+  printf(" ");
+  for (size_t c = 0; c < VECTOR_LEN; c++) {
+    printf("%02x", (const unsigned char)(vec[c]));
+  }
+  printf("\n");
+
+  /*struct aes_key_st *AES_key =*/
+  /*(struct aes_key_st *)malloc(sizeof(struct aes_key_st));*/
+
+  /*if (AES_set_decrypt_key((const unsigned char *)("key"), 256, AES_key) < 0)
+   * {*/
+  /*fprintf(stderr, "Could not set encryption key.");*/
+  /*exit(1);*/
+  /*}*/
+
+  /*unsigned char out[100000];*/
+  /*const char in[i];*/
+  /*AES_decrypt((const unsigned char *)(rest), out, AES_key);*/
+  /*printf("%lu\n", strlen(out));*/
 }
 
 // Extract AES-256-CTR key with embedded python
-const char *calc_aes_key(char *passphrase, int rounds, char *salt) {
+const unsigned char *calc_aes_key(const char *passphrase, const size_t rounds,
+                                  const char *salt) {
   Py_Initialize();
   PyObject *moduleMainString = PyUnicode_FromString("__main__");
   PyObject *moduleMain = PyImport_Import(moduleMainString);
@@ -83,7 +135,7 @@ const char *calc_aes_key(char *passphrase, int rounds, char *salt) {
                      "from Crypto.Hash import HMAC\n\n"
                      "def calc_key(passphrase, iterations, salt):\n"
                      "    key = pbkdf2.PBKDF2(passphrase, salt, iterations, "
-                     "SHA512, HMAC).hexread(64)\n"
+                     "SHA512, HMAC).read(64)\n"
                      "    return key\n");
 
   PyObject *func = PyObject_GetAttrString(moduleMain, "calc_key");
@@ -94,24 +146,24 @@ const char *calc_aes_key(char *passphrase, int rounds, char *salt) {
   PyObject *result = PyObject_CallObject(func, args);
   Py_Finalize();
 
-  return PyUnicode_AsUTF8(result);
+  /*return (const unsigned char *)PyUnicode_AsUTF8(result);*/
+  return (const unsigned char *)PyBytes_AsString(result);
 }
 
-void print_bytes(char *bytes, int len) {
+void print_bytes(const char *bytes, const size_t len) {
   for (int i = 0; i < len; i++) {
     printf("%c", bytes[i]);
   }
 }
 
-void print_hex_bytes(char *bytes, int len) {
+void print_hex_bytes(const char *bytes, const size_t len) {
   for (int i = 0; i < len; i++) {
     printf("%02X", bytes[i]);
   }
 }
 
-void print_uint8(u_int8_t *bytes, int len) {
+void print_uint8(const u_int8_t *bytes, const size_t len) {
   for (int i = 0; i < len; i++) {
     printf("%" PRIu8, bytes[i]);
   }
 }
-
