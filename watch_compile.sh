fdba160c213219cf9f60dae2838c849430b5e3cb
bratekarate <bratekannkarate@gmail.com>
2021-01-21 22:49:42 +0100

    improve watch script

--- watch_compile.sh
+++ watch_compile.sh
@@ -1,14 +1,32 @@
 #!/bin/sh
+SRC=${1:-./src}
 
-inotifywait -m -e modify --include '.*\.c' --format '%w%f' "${1:-./src}" |
+PKGS=
+#PKGS=\
+#nettle,\
+#olm,\
+#openssl,\
+#python3
+
+set --
+OLDIFS=$IFS
+IFS=,
+for PKG in $PKGS; do
+  set -- "$@" $(pkg-config --libs --cflags "$PKG")
+done
+
+#EXTRA_FLAGS=-lpython3
+EXTRA_FLAGS=
+
+for FLAG in $EXTRA_FLAGS; do
+  set -- "$@" "$FLAG"
+done
+IFS=$OLDIFS
+
+inotifywait -m -e modify --include '.*\.c' --format '%w%f' "$SRC" |
   while IFS= read -r F; do
     OUT=out/$(basename "${F%%.c}")
-    tcc -o "$OUT" \
-      $(pkg-config --libs --cflags nettle) \
-      $(pkg-config --libs --cflags olm) \
-      $(pkg-config --libs --cflags python3) -lpython3 \
-      $(pkg-config --libs --cflags openssl) \
-      "$F"
+    tcc -o "$OUT" $@ "$F"
     #"$OUT"
     #printf 'Exit code: %s\n\n' $?
   done
