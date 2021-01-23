75554380466b16e5ce630b7353ee6747a1675f48
bratekarate <bratekannkarate@gmail.com>
2021-01-23 15:01:58 +0100

    include make watcher script

--- watch_make.sh
+++ watch_make.sh
@@ -3,9 +3,7 @@ SRC=${1:-./src}
 
 find "$SRC" -type f -name '*.c' -exec sh -c 'NAME=$(basename "${1%%.c}") make' _ {} \;
 
-#while :; do echo src/socket_client.c; sleep 1; done |
 inotifywait -m -e modify --include '.*\.c' --format '%w%f' "$SRC" |
   while IFS= read -r F; do
-    echo "NAME=$(basename "${F%%.c}") make"
     NAME=$(basename "${F%%.c}") make
   done
