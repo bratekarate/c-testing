de9f4782f599a57139f737b3e519f4154ba112f8
bratekarate <bratekannkarate@gmail.com>
2021-01-24 19:25:09 +0100

    add add_all method to linkedlist

--- src/linked_list.c
+++ src/linked_list.c
@@ -10,9 +10,13 @@ typedef struct LinkedList LinkedList;
 
 // High level functions
 LinkedList *new_list();
-void add(LinkedList *list, const int item);
-void free_list(LinkedList *list);
-void print_list(LinkedList *list);
+void list_add(LinkedList *list, const int item);
+void list_add_all(LinkedList *list, LinkedList *addable);
+int list_get(LinkedList *list, const size_t index);
+int list_remove(LinkedList *list, const size_t index);
+int list_remove_h(LinkedList *list);
+void list_print(LinkedList *list);
+void list_free(LinkedList *list);
 
 // Low level functions
 void init_list(LinkedList **list);
@@ -38,13 +42,31 @@ typedef struct LinkedList {
 int main() {
   LinkedList *list = new_list();
 
-  for(int i = 0; i < 1000000; i++) {
-      add(list, i);
+  for(int i = 0; i < 15; i++) {
+      list_add(list, i);
   }
 
-  print_list(list);
+  list_print(list);
 
-  free_list(list);
+  LinkedList *list2 = new_list();
+
+  for(int i = 500; i < 500 + list -> size; i++) {
+      list_add(list2, i);
+  }
+
+  list_print(list2);
+
+  list_add_all(list, list2);
+
+  list_print(list);
+  printf("list[%d] = %d\nremove %d.\n", 1, 1, list_remove(list, 1));
+  list_print(list);
+  printf("list[%d] = %d\n", 1, list_get(list, 1));
+  printf("list[0] = %d\nremove 0.\n", list_remove_h(list));
+  list_print(list);
+  printf("list[0] = %d\n", list_get(list, 0));
+
+  list_free(list);
   list = NULL;
 
   return EXIT_SUCCESS;
@@ -56,14 +78,72 @@ LinkedList *new_list() {
   return list;
 }
 
-void add(LinkedList *list, const int item) {
+void list_add(LinkedList *list, const int item) {
   Node *node = malloc(NODE_SIZE);
   /*printf("Create node %p\n", node);*/
   add_node(list, &node, item);
 }
 
+void list_add_all(LinkedList *list, LinkedList *addable) {
+   Node *cur = addable -> fst;  
+   while (cur != NULL) {
+       list_add(list, cur -> item);
+       cur = cur -> next;
+   }
+}
+
+// TODO: handle index out of bounds
+int list_get(LinkedList *list, const size_t index){
+    // TODO: if index is past middle, go backwards
+    Node *cur = list -> fst;
+    for(size_t i = 0; i < index; i++) {
+        cur = cur -> next;
+    }
+    return cur -> item;
+}
+
+// TODO: handle index out of bounds
+int list_remove(LinkedList *list, const size_t index){
+    Node *cur = list -> fst;
+    for(size_t i = 0; i < index; i++) {
+        cur = cur -> next;
+    }
+
+    Node *prev = cur -> prev;
+    Node *next = cur -> next;
+    if(prev != NULL) {
+        /*printf("cur -> prev: %d\n", cur -> prev -> item);*/
+        prev -> next = next;
+    }
+
+    if(next != NULL) {
+        /*printf("cur -> next: %d\n", cur -> next -> item);*/
+        next -> prev = prev;
+    }
+
+    if (cur == list -> fst) {
+        list -> fst = cur -> next;
+    }
+
+    if (cur == list -> lst) {
+        list -> lst = cur -> prev;
+    }
+
+    list -> size--;
+
+    int item = cur -> item;
+    free(cur);
+
+    return item;
+}
+
+// TODO: handle list empty
+int list_remove_h(LinkedList *list){
+    return list_remove(list, 0);
+}
+
 // TODO: how to set null
-void free_list(LinkedList *list) {
+void list_free(LinkedList *list) {
     free_nodes(list->lst);
     free(list);
 }
@@ -72,7 +152,7 @@ void free_list(LinkedList *list) {
 void free_nodes(Node *node) {
     while(node != NULL) {
         Node *prev = node->prev;
-        /*printf("Free node %p\n", node);*/
+        /*printf("Free node %p with item %d\n", node, node -> item);*/
         free(node);
         node = prev;
     }
@@ -88,15 +168,19 @@ void free_nodes_rec_broken(Node *node) {
   node = NULL;
 }
 
-void print_list(LinkedList *list) {
-    print_nodes(list->fst);    
+// TODO: make tostring
+void list_print(LinkedList *list) {
+    print_nodes(list->fst);
 }
 
 void print_nodes(Node *node) {
+    printf("[ ");
     while (node != NULL) {
-        printf("%d\n", node->item);
+        char *rest = node -> next == NULL ? " " : ", ";
+        printf("%d%s", node->item, rest);
         node = node -> next;
     }
+    printf("]\n");
 }
 
 void print_nodes_rec_broken(Node *node) {
